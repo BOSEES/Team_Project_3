47c742550ea3ae86d527c17631ae2820a2d9ea2f
김영일 <yeong-il@gim-yeong-il-ui-MacBookAir.local>
2020-07-29 19:23:25 -0700

    team_test2

--- team_test/README.md
+++ team_test/README.md
@@ -1,8 +1,8 @@
-<h1> 서버를 가동하기 전에 init.js 가있는 디렉토리로 들어가서 명령어를 복사 , 붙혀넣기 해주세요</h1>
-npm install
-npm install @babel/core
-npm install @babel/node
-npm install @babel/preset-env
-npm install body-parser
-npm install express ejs --save
-    
+## 서버를 가동하기 전에 init.js 가있는 디렉토리로 들어가서 명령어를 복사 , 붙혀넣기 해주세요
+- [x] npm install 
+- [x] npm install @babel/core
+- [x] npm install @babel/node
+- [x] npm install @babel/preset-env
+- [x]npm install body-parser
+- [x] npm install express ejs --save
+- [x] npm install mysql2    
--- team_test/router/globalRouter.js
+++ team_test/router/globalRouter.js
@@ -1,5 +1,6 @@
 import express from "express";// const express = require("express");
 import routes from "../routes";
+import mysql from "mysql2";
 // import Web3 from "Web3";
 // import product_contract from "'../contract/product/contract.js'"
 
@@ -7,13 +8,39 @@ import routes from "../routes";
 // let smartContract = new web3.eth.Contract(product_contract.abi, product_contract.address);
 
 const globalRouter = express.Router();
+let connection = mysql.createConnection({
+    host: "192.168.0.3",
+    port: 3306, // db 포트
+    user: "block", // user 이름
+    password: "block1234!@", // 비밀번호
+    database: "team_project", // database 이름
+});
+
 
 // home controller
-globalRouter.get(routes.home, (req, res) => res.render("home", {title:"home"}));
+globalRouter.get(routes.home, (req, res) => res.render("home"));
 
 
 // login. controller
 globalRouter.get(routes.login, (req, res) => res.render("login"));
+globalRouter.post(routes.login, (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    connection.query(
+        `SELECT * from users WHERE email=? and password=?`,
+        [email, password],
+        function(err, users) {
+            if ( err ) {
+                console.log(err); // 오류
+            } else if ( users.length > 0 ) { // users 값이 있음
+                req.session.email = email;
+                res.redirect('/');
+            } else { // users 값이 없음 (빈 list)
+                res.render('login', { error: true, email: req.session.email });
+            }
+        }
+    )
+});
 // globalRouter.post(routes.login, (req, res) => {
 //     web3.eth.getAccounts().then(function (accounts) {
 //         var i;
@@ -34,10 +61,55 @@ globalRouter.get(routes.login, (req, res) => res.render("login"));
 
 // logout controller
 globalRouter.get(routes.logout, (req, res) => res.render("logout"));
-
+globalRouter.get(routes.logout, (req, res) => {
+    if (req.session.email) { // 로그인 정보가 있으면 
+        req.session.destroy(function(err) {
+            if ( err ) {
+                console.log(err);
+            } else {
+                res.redirect('/');
+            }
+        })
+    } else {
+        res.redirect('/');
+    }
+});
 
 // join controller
 globalRouter.get(routes.join, (req, res) => res.render("join"));
+globalRouter.post(routes.join, (req, res) => { 
+    const email = req.body.email;
+    const password = req.body.password;
+    var age = null;
+    if ( req.body.age ) {
+        age = req.body.age
+    }
+
+    connection.query(
+        `SELECT id from users WHERE email=?`, // email이 같은 user의 id 찾기
+        [email],
+        function(err, users) {
+            if ( err ) { // 오류 발생
+                res.render('signup', { errorMessage: "오류 발생", email: req.session.email });
+            } else if (users.length > 0) { // 이미 존재하는 이메일
+                res.render('signup', { errorMessage: "이미 존재하는 이메일", email: req.session.email });
+            } else { // email이 `users` table에 없을 경우
+                connection.query(
+                    `INSERT INTO users (email, password, age)
+                        VALUES (?, ?, ?)`,
+                    [email, password, age],
+                    function(err2, result) {
+                        if ( err2 ) { // 오류 발생
+                            res.render('signup', { errorMessage: "생성 오류", email: req.session.email });
+                        } else { // INSERT 성공
+                            res.render('login', { error: false, email: req.session.email });
+                        }
+                    }
+                );
+            }
+        }
+    );
+});
 
 
 // upload controller
